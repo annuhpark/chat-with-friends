5f2d1a77e84bd3e967be39b7f6caf2fb3c9bec71
Anna Park <AnnaPark.Developer@gmail.com>
2022-07-31 12:22:47 -0700

    Chat Application npm start.

--- src/App.js
+++ src/App.js
@@ -1,2 +1,16 @@
 import { ChatEngine } from 'react-chat-engine';
 import './App.css';
+
+
+const App = () => {
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="c2f952ec-70a7-4c99-9d6d-925d182b1d9d"
+      userName="javascriptmastery"
+      userSecret="123123"
+    />
+  )
+}
+
+export default App;
