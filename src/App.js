be0f91d29961d8e28660ffe77d17bb410115e6f1
Anna Park <AnnaPark.Developer@gmail.com>
2022-07-31 15:05:56 -0700

    Removed hardcoding.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import { ChatEngine } from 'react-chat-engine';
 import './App.css';
 import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
 
+const projectID = 'c2f952ec-70a7-4c99-9d6d-925d182b1d9d';
 
 const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+
   return (
     <ChatEngine
       height="100vh"
-      projectID="c2f952ec-70a7-4c99-9d6d-925d182b1d9d"
-      userName="annuhpark"
-      userSecret="Ilovecroissants11!"
-      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps}/>}
+      projectID={projectID}
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
     />
-  )
-}
+  );
+};
+
+// infinite scroll, logout, more customizations...
 
 export default App;
